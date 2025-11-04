13433a137080899a078fbaf38e53962be4b8883b
NaushinK <naushink2709@gmail.com>
2025-11-05 01:46:39 +0530

    FE setup ,redux and routes

---
+++ src/router/AppRouter.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const AppRouter=()=>{
+
+    <Router>
+        <Routes>
+
+        </Routes>
+    </Router>
+}
+export default AppRouter;
